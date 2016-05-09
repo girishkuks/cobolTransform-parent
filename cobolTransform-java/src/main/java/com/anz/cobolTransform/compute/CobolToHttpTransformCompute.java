b71c82cc4d7386cae9f3630e14788da30df50a63
root <root@localhost>
2016-05-09 12:18:06 +1000

    Rename Custom Tarnsform Compute

--- cobolTransform-java/src/main/java/com/anz/cobolTransform/compute/CobolToHttpTransformCompute.java
+++ cobolTransform-java/src/main/java/com/anz/cobolTransform/compute/CobolToHttpTransformCompute.java
@@ -3,13 +3,13 @@ import com.anz.cobolTransform.error.CobolToHttpTransformer;
 import com.anz.cobolTransform.transform.pojo.PurchaseData;
 import com.anz.common.compute.IParser;
 import com.anz.common.compute.TransformType;
-import com.anz.common.compute.impl.CommonCobolTransformCompute;
-import com.anz.common.compute.impl.DFDLParser;
-import com.anz.common.compute.impl.JsonParser;
+import com.anz.common.compute.impl.CustomParserTransformCompute;
+import com.anz.common.compute.impl.JaxbDFDLParser;
+import com.anz.common.compute.impl.JsonBlobParser;
 import com.anz.common.transform.ITransformer;
 
 
-public class CobolToHttpTransformCompute extends CommonCobolTransformCompute<PurchaseData, PurchaseData> {
+public class CobolToHttpTransformCompute extends CustomParserTransformCompute<PurchaseData, PurchaseData> {
 
 	@Override
 	public TransformType getTransformationType() {
@@ -18,12 +18,12 @@ public class CobolToHttpTransformCompute extends CommonCobolTransformCompute<Pur
 
 	@Override
 	public IParser<PurchaseData> getInputParser() {
-		return new DFDLParser<>(PurchaseData.class);
+		return new JaxbDFDLParser<>(PurchaseData.class);
 	}
 
 	@Override
 	public IParser<PurchaseData> getOutputParser() {
-		return new JsonParser<>(PurchaseData.class);
+		return new JsonBlobParser<>(PurchaseData.class);
 	}
 
 	@Override
--- cobolTransform-java/src/main/java/com/anz/cobolTransform/compute/HttpToCobolTransformCompute.java
+++ cobolTransform-java/src/main/java/com/anz/cobolTransform/compute/HttpToCobolTransformCompute.java
@@ -5,12 +5,12 @@ import com.anz.cobolTransform.transform.pojo.CustomerName;
 import com.anz.cobolTransform.transform.pojo.PurchaseData;
 import com.anz.common.compute.IParser;
 import com.anz.common.compute.TransformType;
-import com.anz.common.compute.impl.CommonCobolTransformCompute;
-import com.anz.common.compute.impl.DFDLParser;
-import com.anz.common.compute.impl.JsonParser;
+import com.anz.common.compute.impl.CustomParserTransformCompute;
+import com.anz.common.compute.impl.JaxbDFDLParser;
+import com.anz.common.compute.impl.JsonBlobParser;
 import com.anz.common.transform.ITransformer;
 
-public class HttpToCobolTransformCompute extends CommonCobolTransformCompute<CustomerName, PurchaseData> {
+public class HttpToCobolTransformCompute extends CustomParserTransformCompute<CustomerName, PurchaseData> {
 
 	@Override
 	public TransformType getTransformationType() {
@@ -19,12 +19,12 @@ public class HttpToCobolTransformCompute extends CommonCobolTransformCompute<Cus
 
 	@Override
 	public IParser<CustomerName> getInputParser() {
-		return new JsonParser<>(CustomerName.class);
+		return new JsonBlobParser<>(CustomerName.class);
 	}
 
 	@Override
 	public IParser<PurchaseData> getOutputParser() {
-		return new DFDLParser<>(PurchaseData.class);
+		return new JaxbDFDLParser<>(PurchaseData.class);
 	}
 
 	@Override
