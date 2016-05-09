9c2c6708a08721e9790137eab40a998e840a6aca
root <root@localhost>
2016-05-09 17:05:17 +1000

    Error failure flow test

--- cobolTransform-java/src/main/java/com/anz/cobolTransform/transform/HttpToCobolTransformer.java
+++ cobolTransform-java/src/main/java/com/anz/cobolTransform/transform/HttpToCobolTransformer.java
@@ -18,39 +18,38 @@ import com.anz.common.transform.TransformUtils;
 
 /**
  * @author sanketsw
- *
+ * 
  */
-public class HttpToCobolTransformer implements ITransformer<CustomerName, PurchaseData> {
+public class HttpToCobolTransformer implements
+		ITransformer<CustomerName, PurchaseData> {
 
 	@Override
 	public PurchaseData execute(CustomerName input, Logger appLogger,
 			ComputeInfo metadata) throws Exception {
-		
+
 		PurchaseData p = new PurchaseData();
-		if(input != null) {
-			p.setCustomerSurname(input.getSurname());				
+		if (input == null || input.getSurname() == null) {
+			throw new NullPointerException("Invalid input");
 		}
-		if(p.getCustomerSurname() == null) {
-			p.setCustomerSurname("Sangwikar");
-		}
-		
+		p.setCustomerSurname(input.getSurname());
+
 		p.getCustomerInitial().add("s");
 		p.getCustomerInitial().add("d");
-		
+
 		p.setItemCount(1);
 		p.setInvoice(new Invoice());
-		
+
 		ItemGrocery i = new ItemGrocery();
 		i.setDescription("Winged Bovine Power Drink");
 		i.setItemCode("012RBU0002");
 		i.setPrice(new BigDecimal(2.99));
 		i.setQuantity(6);
 		i.setSellByDate(20150731);
-		
+
 		Item item = new Item();
 		item.setItemType("G");
 		item.setItemGrocery(i);
-		
+
 		p.getInvoice().getItem().add(item);
 		return p;
 	}
